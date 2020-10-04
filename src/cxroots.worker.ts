1da4ce3c201ddc820bb11ca4b186c088cdd88323
Robert Parini <robert.parini@gmail.com>
2020-10-04 22:20:12 +0100

    Import cxroots in webworker

--- src/cxroots.worker.ts
+++ src/cxroots.worker.ts
@@ -1,38 +1,36 @@
 
-console.log('hello from a webworker');
+console.log('Loading pyodide')
+importScripts("pyodide.js");
+console.log('Loaded pyodide')
 
-addEventListener('message', (message) => {
+declare var languagePluginLoader: Promise<any>;
+declare var pyodide: {
+    runPython(python: string): string;
+    runPythonAsync(python: string): Promise<string>;
+    pyimport<T>(variableName: string): T;
+    loadPackage(package: string | string[]): Promise<void>;
+};
+
+const pythonCode = `
+import micropip
+def do_work(*args):
+    import cxroots
+    print('Imported cxroots')
+micropip.install([
+    'https://files.pythonhosted.org/packages/62/2a/31e70724b12d7ddf776960c219219f5035f2cf21e2f3e132f584b513e200/cxroots-1.1.10-py3-none-any.whl',
+    'https://files.pythonhosted.org/packages/ab/c0/b0d967160ecc8db52ae34e063937d85e8d386f140ad4826aae2086245a5e/numdifftools-0.9.39-py2.py3-none-any.whl',
+    'https://files.pythonhosted.org/packages/60/1d/9e398c53d6ae27d5ab312ddc16a9ffe1bee0dfdf1d6ec88c40b0ca97582e/numpydoc-1.1.0-py3-none-any.whl',
+]).then(do_work)
+`
+
+async function runCxroots(): Promise<any> {
+    await languagePluginLoader;
+    await pyodide.loadPackage(['micropip', 'numpy', 'scipy']);
+    pyodide.runPython(pythonCode)
+}
+
+addEventListener('message', async (message) => {
     console.log('in webworker', message);
+    await runCxroots()
     postMessage('this is the response ' + message.data);
 });
-
-// importScripts("pyodide.js");
-
-// declare var languagePluginLoader: Promise<any>;
-// declare var pyodide: {
-//     runPython(python: string): string;
-//     runPythonAsync(python: string): Promise<string>;
-//     pyimport<T>(variableName: string): T;
-//     loadPackage(package: string | string[]): Promise<void>;
-// };
-
-// const pythonCode = `
-// def do_work(*args):
-//     import cxroots
-//     print('Imported cxroots')
-
-// import micropip
-// micropip.install([
-//     'https://files.pythonhosted.org/packages/62/2a/31e70724b12d7ddf776960c219219f5035f2cf21e2f3e132f584b513e200/cxroots-1.1.10-py3-none-any.whl',
-//     'https://files.pythonhosted.org/packages/ab/c0/b0d967160ecc8db52ae34e063937d85e8d386f140ad4826aae2086245a5e/numdifftools-0.9.39-py2.py3-none-any.whl',
-//     'https://files.pythonhosted.org/packages/60/1d/9e398c53d6ae27d5ab312ddc16a9ffe1bee0dfdf1d6ec88c40b0ca97582e/numpydoc-1.1.0-py3-none-any.whl',
-// ]).then(do_work)
-// `
-
-// async function runCxroots(): Promise<any> {
-//     await languagePluginLoader;
-//     await pyodide.loadPackage(['micropip', 'numpy', 'scipy'])
-//     pyodide.runPython(pythonCode)
-// }
-
-// export { runCxroots }
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,10 @@
 import { HelloWorldInTypescript } from './hello-world.class'
 HelloWorldInTypescript.HelloWorld()
 
-
 console.log('starting up');
 
-
 import * as workerPath from "file-loader?name=[name].js!./cxroots.worker";
-console.log(workerPath);
 const worker = new Worker(workerPath);
-console.log('started worker');
 
 worker.addEventListener('message', message => {
     console.log(message);
