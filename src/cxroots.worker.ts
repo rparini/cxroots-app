191164548d28a1fb888751379a05f83bc1b92905
Robert Parini <robert.parini@gmail.com>
2020-10-05 21:41:38 +0100

    Add comment

--- src/cxroots.worker.ts
+++ src/cxroots.worker.ts
@@ -16,7 +16,7 @@ import micropip
 def do_work(*args):
     import cxroots
     print('Imported cxroots')
-    return 42
+    return 42 # This gets returned by pyodide.runPython
 micropip.install([
     'https://files.pythonhosted.org/packages/62/2a/31e70724b12d7ddf776960c219219f5035f2cf21e2f3e132f584b513e200/cxroots-1.1.10-py3-none-any.whl',
     'https://files.pythonhosted.org/packages/ab/c0/b0d967160ecc8db52ae34e063937d85e8d386f140ad4826aae2086245a5e/numdifftools-0.9.39-py2.py3-none-any.whl',
