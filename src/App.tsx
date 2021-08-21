9b51e60011f089ce222ad30fbde67db7ebc850ae
Robert Parini <robert.parini@gmail.com>
2021-08-21 16:57:45 +0100

    Add text form to take user input and x2

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,82 @@
-import * as React from "react"
-import * as workerPath from "file-loader?name=[name].js!./cxroots.worker"
+import * as React from "react";
+import * as workerPath from "file-loader?name=[name].js!./cxroots.worker";
 
-const worker = new Worker(workerPath)
+const worker = new Worker(workerPath);
 
-export class App extends React.Component<{}, { status: string , result: string}> {
+export class App extends React.Component<
+  {},
+  { status: string; inputFunction: string; result: string }
+> {
   constructor(props) {
-    super(props)
-    this.state = {status: "Fresh", result: null}
+    super(props);
+    this.state = { status: "Fresh", inputFunction: null, result: null };
 
     // This binding is necessary to make `this` work in the callback
     this.findRoots = this.findRoots.bind(this);
   }
 
-  findRoots() {
-    this.setState({status: "Running"})
-    worker.postMessage({f: '2'})
-    worker.addEventListener('message', message => {
-      console.log('message returned:')
-      console.log(message)
-      this.setState({status: "Finished", result: message.data})
-    })
+  findRoots(args: object) {
+    this.setState({ status: "Running" });
+    worker.postMessage(args);
+    worker.addEventListener("message", (message) => {
+      console.log("message returned:");
+      console.log(message);
+      this.setState({ status: "Finished", result: message.data });
+    });
   }
 
   public render() {
     return (
       <div className="App">
         <h1>Hello there!</h1>
-        <Button onClick={this.findRoots} />
+        <RootFindingForm onSubmit={this.findRoots} />
         <Results status={this.state.status} />
         The answer is: {this.state.result}
       </div>
-    )
-  }
-}
-
-class Button extends React.Component<{onClick: any}, {}> {
-  render() {
-    return (
-      <button onClick={this.props.onClick}>
-        Run
-      </button>
     );
   }
 }
 
-class Results extends React.Component<{'status':string}, {}> {
-  public render() {
-    return <h3>The status is: {this.props.status}</h3>
+class RootFindingForm extends React.Component<
+  { onSubmit: any },
+  { targetFunc: string }
+> {
+  constructor(props) {
+    super(props);
+    this.state = { targetFunc: "" };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ targetFunc: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault(); // To prevent page from refreshing on submit
+    this.props.onSubmit({ f: this.state.targetFunc });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          f(x):
+          <input
+            type="text"
+            value={this.state.targetFunc}
+            onChange={this.handleChange}
+          />
+        </label>
+        <input type="submit" value="x2" />
+      </form>
+    );
   }
 }
 
+class Results extends React.Component<{ status: string }, {}> {
+  public render() {
+    return <h3>The status is: {this.props.status}</h3>;
+  }
+}
--- src/cxroots.worker.ts
+++ src/cxroots.worker.ts
@@ -1,7 +1,6 @@
-
-console.log('Loading pyodide')
-importScripts("pyodide.js")
-console.log('Loaded pyodide')
+console.log("Loading pyodide");
+importScripts("pyodide.js");
+console.log("Loaded pyodide");
 
 declare var languagePluginLoader: Promise<any>;
 declare var pyodide: {
@@ -9,7 +8,7 @@ declare var pyodide: {
     runPythonAsync(python: string): Promise<string>;
     pyimport<T>(variableName: string): T;
     loadPackage(package: string | string[]): Promise<void>;
-}
+};
 
 const pythonCode = `
 import micropip
@@ -19,32 +18,32 @@ def do_work(*args):
     print('doing work')
     import cxroots
     print('finished imports')
-    return f # This gets returned by pyodide.runPython
+    return 2*float(f) # This gets returned by pyodide.runPython
 micropip.install([
     'https://files.pythonhosted.org/packages/62/2a/31e70724b12d7ddf776960c219219f5035f2cf21e2f3e132f584b513e200/cxroots-1.1.10-py3-none-any.whl',
     'https://files.pythonhosted.org/packages/ab/c0/b0d967160ecc8db52ae34e063937d85e8d386f140ad4826aae2086245a5e/numdifftools-0.9.39-py2.py3-none-any.whl',
     'https://files.pythonhosted.org/packages/60/1d/9e398c53d6ae27d5ab312ddc16a9ffe1bee0dfdf1d6ec88c40b0ca97582e/numpydoc-1.1.0-py3-none-any.whl',
 ]).then(do_work)
-`
+`;
 
 async function runCxroots(message): Promise<any> {
-    await languagePluginLoader
-    await pyodide.loadPackage(['micropip', 'numpy', 'scipy'])
+    await languagePluginLoader;
+    await pyodide.loadPackage(["micropip", "numpy", "scipy"]);
 
     const data = message.data;
-    console.log(data)
+    console.log(data);
     const keys = Object.keys(data);
     for (let key of keys) {
         // Set keys on self, so that `from js import key` works.
-        console.log('setting', key, data[key])
+        console.log("setting", key, data[key]);
         self[key] = data[key];
     }
     return pyodide.runPython(pythonCode);
 }
 
-addEventListener('message', async (message) => {
-    console.log('message send to worker:' + message)
-    var result = await runCxroots(message)
+addEventListener("message", async (message) => {
+    console.log("message send to worker:" + message);
+    var result = await runCxroots(message);
     // send message back to the main thread
-    postMessage('this is the response: ' + result)
-})
+    postMessage("this is the response: " + result);
+});
