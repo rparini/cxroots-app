37081513bdbd56d8f1675eeed33aeeef60ff0e90
Robert Parini <robert.parini@gmail.com>
2022-09-13 23:44:26 +0100

    Fix react-plotly import

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,6 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
+// Fix react-plotly import: https://github.com/plotly/react-plotly.js/issues/115
+import 'jest-canvas-mock';
+window.URL.createObjectURL = function() {};
