289b060233aab0148dfeb4109c7f8dec6a9554e4
Robert Parini <robert.parini@gmail.com>
2022-09-16 01:48:20 +0100

    Format code

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
 // test('renders without crashing', () => {
 //   const div = document.createElement('div');
