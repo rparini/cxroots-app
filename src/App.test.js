67c7aaccd650a2a2b927ff25d47f943bea87d3ec
Robert Parini <robert.parini@gmail.com>
2022-09-18 01:04:07 +0100

    Load pyodide with scriptjs

--- src/App.test.js
+++ src/App.test.js
@@ -3,10 +3,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-// test('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<App />, div);
-// });
+test("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<App />, div);
+});
 
 // test("renders cxroots", () => {
 //   render(<App />);
