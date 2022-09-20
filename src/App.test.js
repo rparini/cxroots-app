97a78b97496e4c7e6a8d6a3f9813b74adaaf402d
Robert Parini <robert.parini@gmail.com>
2022-09-20 21:50:10 +0100

    Add commented out test

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,17 @@ test("renders page", () => {
   const linkElement = screen.getByText("cxroots");
   expect(linkElement).toBeInTheDocument();
 });
+
+/** making any actual remote call times out in Jest */
+// test("runCxroots", () => {
+//   const pythonArgs = {
+//     function_string: "(z-1)^2",
+//     circle_center: 0,
+//     circle_radius: 3,
+//   };
+//   return loadPyodide()
+//     .then((pyodide) => runCxroots(pyodide, pythonArgs))
+//     .then((result) => {
+//       expect(result).toBe({ roots: [1], multiplicities: [2] });
+//     });
+// }, 600000);
